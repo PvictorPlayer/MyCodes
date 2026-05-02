ef5cb95014b0af6c41a877c786d0b4fe4a70a0dd
PvictorPlayer <pvictor.rocha8@gmail.com>
2026-05-02 14:21:57 -0300

    alteração semi final

--- tabela_prática.py
+++ tabela_prática.py
@@ -74,7 +74,7 @@ class aplicativo(ctk.CTk):
         self.saudeget = self.saude.get()
         self.educacaoget = self.educacao.get()
         if self.prefeituraget != "":
-            pref_int = (self.prefeituraget)
+            pref_int = (self.prefeituraget) 
 
         if self.assistenciaget != "":
             assis_int = (self.assistenciaget)
@@ -86,10 +86,29 @@ class aplicativo(ctk.CTk):
             educ_int = (self.educacaoget)
 
         pref_int = pref_int.replace(',', '.')
+        pref_int = list(map(float, pref_int.split()))
+        self.preftotal = sum(pref_int)
+
+        assis_int = assis_int.replace(',', '.')
+        assis_int = list(map(float, assis_int.split()))
+        self.assistotal = sum(assis_int)
+        
+        saude_int = saude_int.replace(',', '.')
+        saude_int = list(map(float, saude_int.split()))
+        self.saudetotal = sum(saude_int)
+
+        educ_int = educ_int.replace(',', '.')
+        educ_int = list(map(float, educ_int.split()))
+        self.eductotal = sum(educ_int)
+
+        self.pref.configure(text=f"Prefeitura: {self.preftotal}")
+        self.assis.configure(text=f"Assistência: {self.assistotal}")
+        self.sau.configure(text=f"Saúde: {self.saudetotal}")
+        self.educ.configure(text=f"Educação: {self.eductotal}")
 
     def botao(self):
         self.button = ctk.CTkButton(self.principal,
-                               text="enviar",
+                               text="Solicitar",
                                command=self.pegar)
         self.button.pack()
     
